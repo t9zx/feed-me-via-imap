24d5b315bdd2b350d5c14702d146ce4d374e1678
t9zx <t9zx@github.com>
2013-12-27 21:49:28 +0100

    Processing all feeds as given in the config file

--- lib/feed_me/feed_me.rb
+++ lib/feed_me/feed_me.rb
@@ -28,7 +28,6 @@ module FeedMe
         end
         logger.debug{"Config file read"}
 
-        ##### If you are in doubt, try it out ######
         imap = FeedMe::Processor::ImapSyncer.new(
             conf.value("imap.server"),
             conf.value("imap.user"),
@@ -38,12 +37,22 @@ module FeedMe
         )
         imap.login
 
-        feed = FeedMe::Model::Feed.new(URI("http://www.heise.de/newsticker/heise-atom.xml"))
-        FeedMe::Processor::FeedReader.retrieve_feed(feed)
-        feed.feed_items.each do |feed_item|
-          imap.store_item(feed_item, "Test.Manual")
+        # process each feed from the config file
+        # TODO - make this more failsafe
+        feeds = conf.value("feeds")
+        feeds.each do |feed_config|
+          feed = FeedMe::Model::Feed.new(URI(feed_config['url']))
+          FeedMe::Processor::FeedReader.retrieve_feed(feed)
+          feed.feed_items.each do |feed_item|
+            imap.store_item(feed_item, feed_config['folder'])
+          end
         end
-          ##### I hope all doubts are gone now ######
+
+        #feed = FeedMe::Model::Feed.new(URI("http://www.heise.de/newsticker/heise-atom.xml"))
+        #FeedMe::Processor::FeedReader.retrieve_feed(feed)
+        #feed.feed_items.each do |feed_item|
+        #  imap.store_item(feed_item, "Test.Manual")
+        #end
       ensure
         imap.logout if imap && imap.logged_in?
       end
